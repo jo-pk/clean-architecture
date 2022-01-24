3c5212d6c56c463561650a9db6a6bc85db384660
seovalue <seominjeong.dev@gmail.com>
2022-01-24 11:10:52 +0900

    etc: initialize project structure

---
+++ src/main/java/com/jopk/cleanarchitecture/domain/Account.java
@@ -0,0 +1,4 @@
+package com.jopk.cleanarchitecture.domain;
+
+public class Account {
+}
---
+++ src/main/java/com/jopk/cleanarchitecture/presentation/BuckpalController.java
@@ -0,0 +1,7 @@
+package com.jopk.cleanarchitecture.presentation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BuckpalController {
+}
